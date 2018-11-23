327b9c0c521dd073fd6cb4eb3af402c9286c16d3
SlientAutumn <280729833@qq.com>
2018-11-23 22:00:52 +0800

    dsa

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="main">
-		<img :src="imgsrc" alt="">
+		<img :src="data.goods_pic_url" alt="">
 		<div class="intro">
 			<p class="price">￥
 				<span class="nowprice">{{data.cprice}}</span>
@@ -9,6 +9,46 @@
 				<span class="number">{{data.join_number}}</span>
 			</p>
 			<p class="langname">{{name}}</p>
+			<ul>
+				<li class="discount"> 
+					<span v-if="discount[0]" class="discount-title">{{discount[0].title}}</span>
+					<span v-if="discount[0]" class="discount-content">{{discount[0].content}}</span>
+				</li>
+				<li class="promise"> 
+					<span>24小时发货</span>
+					<span>7天包换</span>
+					<span>售后补贴</span>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<span>已选择</span> 
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<span>图文详情</span> 
+				</li>
+			</ul>
+			<div>
+				<img v-for="av in sku" :src="av.av_origin_zpic" alt="">
+				
+			</div>
+			
+			<div class="shoppingcart" v-if="isShow">
+				<div class="shoppingImg">
+					<img :src="data.goods_pic_url" alt="">
+				</div>
+				<span>单价</span>
+				<span class="nowprice">{{data.cprice}}</span>
+				<span>购买数量</span>
+				<button  @click="count++">+</button>
+				<span>{{count}}</span>
+
+				<button @click="count===1?1:count--">-</button>
+				<p class="makeSure">确定</p>
+				<button class="close" @click="isShow=false">x</button>
+			</div>
 		</div>
 		<footer>
 			<div class="footfirst">
@@ -22,7 +62,7 @@
 			<div class="footthird">
 				<p>立即购买</p>
 			</div>
-			<div class="footfour">
+			<div class="footfour" @click="isShow=true">
 				<p>加入购物车</p>
 			</div>
 		</footer>
@@ -36,16 +76,22 @@
 		data(){
 			return {
 				data:{},
-				id:"90166308",
+				discount:[],
 				name:null,
 				imgsrc:null,
-				count:0
+				count:1,
+				sku:[],
+				isShow:false
 			}
 		},
 		mounted(){
-			this.id=this.$route.params.id
-			axios.get("/api/getMemberAboutInfo?goods_id=90166308").then(res=>{
+			console.log(this.$route.params.id.replace(":",""))
+			axios.get(`/api/getMemberAboutInfo?goods_id=${this.$route.params.id.replace(":","")}`).then(res=>{
+				console.log(res.data)
 				this.data = res.data.skudata.info;
+				this.discount = res.data.discount.coupon;
+				console.log(res.data.skudata.sku)
+				this.sku=res.data.skudata.sku;
 			}).catch(err=>{
 				console.log(err)
 			})
@@ -67,6 +113,14 @@
 	}
 </script>
 <style scoped lang="scss">
+
+*{
+	margin:0;
+	padding:0;
+}
+ul,li{
+	list-style:none;
+}
 .main{
 	background:lightgrey;
 }
@@ -103,6 +157,60 @@
 		.langname{
 			line-height:20px;
 		}
+		ul{
+			margin-top:20px;
+			.discount{
+				
+				font-size:14px;
+				height:30px;
+				line-height:30px;
+				margin:0 5px;
+				border-bottom:1px solid #666;
+				.discount-title{
+					background: red;
+					color: white;
+				}
+			}
+			.promise{
+				
+				font-size:12px;
+				height:30px;
+				line-height:30px;
+				margin:0 5px;
+				
+			}
+		}
+		.shoppingcart{
+			width:100%;
+			height:300px;
+			position:fixed;
+			background:white;
+			left:0;
+			bottom:0;
+			z-index:10;
+			.shoppingImg{
+				width:100px;
+				height:100px;
+				img{
+					width:100%;
+					height:100%;
+				}
+			}
+			.makeSure{
+				position:absolute;
+				bottom:0;
+				height:50px;
+				background:red;
+				width:100%;
+				text-align:center;
+			}
+			.close{
+				position:absolute;
+				top:0;
+				right:0;
+			}
+		}
+		
 	}
 	footer{
 		width:100%;
@@ -111,6 +219,7 @@
 		position:fixed;
 		left:0;
 		bottom:0;
+		
 		.footfirst,.footsecond,.footthird,.footfour{
 			float:left;
 			text-align:center;
@@ -140,6 +249,7 @@
 			width:130px;
 			background:red;
 			color:white;
+
 			p{
 				line-height:50px;
 			}
